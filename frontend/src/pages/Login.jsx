2dff0a2548b8fa36e6c69664ea225e83e010aeec
saisuvanth <saisuvanth@gmail.com>
2022-09-05 17:51:02 +0530

    hmmm

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { Container, Grid, Image } from '@mantine/core'
+import React from 'react'
+import img from '../assets/img/30499.jpg';
+
+const Login = () => {
+	return (
+		<Container p={0} m={0} sx={{ width: '100%' }}>
+		</Container>
+	)
+}
+
+export default Login
