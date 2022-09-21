712cea2c67efe582b258f85584fa3f5bf07320fa
Shardul Kurdukar <24shardul@gmail.com>
2022-09-22 02:59:52 +0530

    react register some

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import { MantineProvider } from '@mantine/core';
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import theme from './utils/theme';
-import Home from './pages/Home';
+import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import { NotificationsProvider } from "@mantine/notifications";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import theme from "./utils/theme";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-      <div className="App">
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/*' element={<Home />} />
-        </Routes>
-      </div>
+      <NotificationsProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/*" element={<Home />} />
+          </Routes>
+        </div>
+      </NotificationsProvider>
     </MantineProvider>
   );
 }
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,9 +1,10 @@
-export const SET_CUR_PATH = 'SET_CUR_PATH';
-export const SET_NAVBAR = 'SET_NAVBAR';
-export const SET_PAIR_MODAL = 'SET_PAIR_MODAL';
+export const SET_CUR_PATH = "SET_CUR_PATH";
+export const SET_NAVBAR = "SET_NAVBAR";
+export const SET_PAIR_MODAL = "SET_PAIR_MODAL";
+export const DOMAIN_NAME = "http://localhost:3001/api";
 
 export const defaultHomeState = {
-	currPath: '/',
-	navbar: false,
-	pairModal: false,
-}
+  currPath: "/",
+  navbar: false,
+  pairModal: false,
+};
